da01d4fa719101c8694d4c3284ba8eb6245b5abd
syrsas <87002583+syrsas@users.noreply.github.com>
2021-07-30 22:42:14 +0900

    ファイル作成
    基本課題

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -6,13 +6,18 @@ using UnityEngine.UI;
 public class BallController : MonoBehaviour
 {
     private float visiblePosZ = -6.5f;
-
+    // Game Over 表示
     private GameObject gameoverText;
+    // 得点表示
+    private GameObject scoreText;
+    // ボールの衝突時に加算される点数
+    private int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.gameoverText = GameObject.Find("GameOverText");    
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.scoreText = GameObject.Find("ScoreText");
     }
 
     // Update is called once per frame
@@ -21,6 +26,30 @@ public class BallController : MonoBehaviour
         if(this.transform.position.z < this.visiblePosZ)
         {
             this.gameoverText.GetComponent<Text>().text = "Game Over";
+            this.scoreText.GetComponent<Text>().text = score.ToString();
+
+        }
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        // デバッグ
+        Debug.Log("衝突した" + other.gameObject.tag);
+
+        // タグによって光らせる色を変える
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            score += 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag" || other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 30;
         }
     }
 }
+
